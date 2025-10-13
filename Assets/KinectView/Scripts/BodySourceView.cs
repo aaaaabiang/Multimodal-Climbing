595a94c5c83d733e3181c160a6eaa83b75e020c6
Shawn <aaaaabiang@gmail.com>
2025-10-13 23:22:10 +0800

    add scene C with resonance audio

--- Assets/KinectView/Scripts/BodySourceView.cs
+++ Assets/KinectView/Scripts/BodySourceView.cs
@@ -21,8 +21,13 @@ public class BodySourceView : MonoBehaviour
     {
         JointType.HandLeft,
         JointType.HandRight,
+        JointType.FootLeft,
+        JointType.FootRight,
         JointType.Head,
     };
+
+    private AudioListener mAudioListener;
+
 
     //private Dictionary<Kinect.JointType, Kinect.JointType> _BoneMap = new Dictionary<Kinect.JointType, Kinect.JointType>()
     //{
@@ -30,32 +35,42 @@ public class BodySourceView : MonoBehaviour
     //    { Kinect.JointType.AnkleLeft, Kinect.JointType.KneeLeft },
     //    { Kinect.JointType.KneeLeft, Kinect.JointType.HipLeft },
     //    { Kinect.JointType.HipLeft, Kinect.JointType.SpineBase },
-        
+
     //    { Kinect.JointType.FootRight, Kinect.JointType.AnkleRight },
     //    { Kinect.JointType.AnkleRight, Kinect.JointType.KneeRight },
     //    { Kinect.JointType.KneeRight, Kinect.JointType.HipRight },
     //    { Kinect.JointType.HipRight, Kinect.JointType.SpineBase },
-        
+
     //    { Kinect.JointType.HandTipLeft, Kinect.JointType.HandLeft },
     //    { Kinect.JointType.ThumbLeft, Kinect.JointType.HandLeft },
     //    { Kinect.JointType.HandLeft, Kinect.JointType.WristLeft },
     //    { Kinect.JointType.WristLeft, Kinect.JointType.ElbowLeft },
     //    { Kinect.JointType.ElbowLeft, Kinect.JointType.ShoulderLeft },
     //    { Kinect.JointType.ShoulderLeft, Kinect.JointType.SpineShoulder },
-        
+
     //    { Kinect.JointType.HandTipRight, Kinect.JointType.HandRight },
     //    { Kinect.JointType.ThumbRight, Kinect.JointType.HandRight },
     //    { Kinect.JointType.HandRight, Kinect.JointType.WristRight },
     //    { Kinect.JointType.WristRight, Kinect.JointType.ElbowRight },
     //    { Kinect.JointType.ElbowRight, Kinect.JointType.ShoulderRight },
     //    { Kinect.JointType.ShoulderRight, Kinect.JointType.SpineShoulder },
-        
+
     //    { Kinect.JointType.SpineBase, Kinect.JointType.SpineMid },
     //    { Kinect.JointType.SpineMid, Kinect.JointType.SpineShoulder },
     //    { Kinect.JointType.SpineShoulder, Kinect.JointType.Neck },
     //    { Kinect.JointType.Neck, Kinect.JointType.Head },
     //}; 
-    
+
+    void Start()
+    {
+        // 检查场景中是否已经存在一个 Audio Listener
+        if (FindObjectOfType<AudioListener>() != null && mAudioListener == null)
+        {
+            Debug.LogWarning("场景中已经存在一个 Audio Listener。");
+            return;
+        }
+    }
+
     void Update () 
     {
         #region Get Kinect data
@@ -181,6 +196,25 @@ public class BodySourceView : MonoBehaviour
             //get joint,set nwe position
             Transform jointObject = bodyObject.transform.Find(_joint.ToString());
             jointObject.position = targetPosition;
+
+            // 如果是 Head 关节，将 Audio Listener 移动到该位置
+            if (_joint == JointType.Head)
+            {
+                if (mAudioListener == null)
+                {
+                    // 如果没有 Audio Listener，则创建一个并附加到 Head 关节
+                    mAudioListener = jointObject.gameObject.AddComponent<AudioListener>();
+                }
+                else
+                {
+                    // 如果已有 Audio Listener，则更新其位置
+                    mAudioListener.transform.position = targetPosition;
+
+                    // 反转 Audio Listener 的方向，使其背对摄像机
+                    mAudioListener.transform.rotation = Quaternion.LookRotation(-Camera.main.transform.forward, Vector3.up);
+
+                }
+            }
         }
     //    for (Kinect.JointType jt = Kinect.JointType.SpineBase; jt <= Kinect.JointType.ThumbRight; jt++)
     //    {
